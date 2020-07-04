ebd80f5fd9fa4435aba1f62b132780c3db30d13f
perfree <yin741223257>
2020-07-04 10:11:32 +0800

    :smile: 文章发表

--- perfree-web/src/main/java/com/perfree/config/WebConfig.java
+++ perfree-web/src/main/java/com/perfree/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.perfree.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,12 +14,14 @@ import java.util.List;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${upload.imgPath}")
+    private String uploadImgPath;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-        registry.addResourceHandler("/img/**").addResourceLocations("file:F:/test/");
+        registry.addResourceHandler("/img/**").addResourceLocations("file:" + uploadImgPath);
 
     }
 
